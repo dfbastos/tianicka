24b8b86f23a058ef13327dff58fec5de81cdfc13
unknown <Diego@Diego.Freitas>
2020-08-31 16:59:13 -0300

    Incluindo SysAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Alunos, Turmas, Estrelas
 
 # Register your models here.
+
+class Aluno(admin.ModelAdmin):
+    list_display = ['nome', 'turma']
+    search_fields = []
+
+
+class Turma(admin.ModelAdmin):
+    list_display = ['turma']
+    search_fields = []
+
+
+class Estrela(admin.ModelAdmin):
+    list_display = ['data', 'aluno']
+    search_fields = []
+
+
+admin.site.register(Alunos, Aluno)
+admin.site.register(Turmas, Turma)
+admin.site.register(Estrelas, Estrela)
